f22c2866a95921e671941307568ad3c6b4380c8d
Ahmad Hajjar <ahmed_hajjar@hotmail.com>
2022-11-12 22:02:29 +0100

    Removed unnecessary erase function from Thing class

--- src/main/kotlin/app/objects/Thing.kt
+++ src/main/kotlin/app/objects/Thing.kt
@@ -35,24 +35,12 @@ class Thing(
         }
 
     fun draw(builder: StringBuilder) {
-
-        erase(builder)
         builder
             .append("\u001b[${position.posY};${position.posX}H")
             .append("\u001b[38:5:${color}m")
             .append("\u25E2\u25E3")
             .append("\u001b[${position.posY + 1};${position.posX}H")
             .append("\u25E5\u25E4")
-            .append("\u001b[0m")
-    }
-
-    private fun erase(builder: StringBuilder) {
-        builder
-            .append("\u001b[${position.posY};${position.posX + 1}H")
-            .append("\u001b[30m")
-            .append("  ")
-            .append("\u001b[${position.posY + 1};${position.posX + 1}H")
-            .append("  ")
             .append("\u001b[0m")
     }
 
